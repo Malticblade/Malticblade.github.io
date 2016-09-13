04b1b74e051acd0e211a527dfdbc6e939da8678c
Steven Green <sesoccer911@aol.com>
2016-09-13 11:51:40 -0500

    First batch of files
    Used underscores.me

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Malticblade.github.io
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'malticblade-github-io' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'malticblade-github-io' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'malticblade-github-io' ), 'malticblade-github-io', '<a href="http://underscores.me/" rel="designer">Malticblade</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Malticblade.github.io Theme Customizer.
+ *
+ * @package Malticblade.github.io
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function malticblade_github_io_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'malticblade_github_io_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function malticblade_github_io_customize_preview_js() {
+	wp_enqueue_script( 'malticblade_github_io_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'malticblade_github_io_customize_preview_js' );
